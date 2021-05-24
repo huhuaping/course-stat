2baec7179478e5503ddf4476350066d5e1ce123d
huhuaping <huhuaping01@nwsuaf.edu.cn>
2021-05-24 21:59:49 +0800

    individual index

--- 03-slide-class/Rscript/case-index.R
+++ 03-slide-class/Rscript/case-index.R
@@ -6,7 +6,9 @@ calc_index <- df_index %>%
   mutate(q0p0 = q_0 * p_0,
          q0p1 = q_0 * p_1,
          q1p1 = q_1 * p_1,
-         q1p0 = q_1 * p_0) 
+         q1p0 = q_1 * p_0,
+         k_q = round(q_1/q_0,4),
+         k_p = round(p_1/p_0,4)) 
 sum_00 <- sum(calc_index$q0p0)
 sum_01 <- sum(calc_index$q0p1)
 sum_11 <- sum(calc_index$q1p1)
@@ -24,9 +26,13 @@ names_chn <- c("产品名称", "单位",
                "产量\\(q_0\\)","产量\\(q_1\\)",
                "价格\\(p_0\\)","价格\\(p_1\\)",
                "\\(q_0p_0\\)","\\(q_0p_1\\)",
-               "\\(q_1p_1\\)","\\(q_1p_0\\)")
+               "\\(q_1p_1\\)","\\(q_1p_0\\)",
+               "\\(k_q\\)", "\\(k_p\\)")
 tbl_index <- 
   calc_index  %>%
   janitor::adorn_totals(.,where = "row",name = c("合计")) %>%
   mutate_at(vars(contains("_")), funs(ifelse(.==max(.), NA, .)))%>%
   rename_at(vars(names(.)),~ all_of(names_chn)) 
+
+# individual index case
+
