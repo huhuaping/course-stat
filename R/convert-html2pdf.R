2e6b4e7df19ea51fd8a4685ca2022873e267ae4d
huhuaping <huhuaping01@nwsuaf.edu.cn>
2022-02-21 15:30:41 +0800

    pdftopptx fixed

---
+++ R/convert-html2pdf.R
@@ -0,0 +1,26 @@
+# see [yihui](https://github.com/rstudio/pagedown/issues/68#issuecomment-462032816)
+
+require("pagedown")
+
+file_dir <- "d://github/course-stat/03-slide-class/"
+file_list <- list.files(file_dir)
+id_target <- which(stringr::str_detect(file_list, "^[[\\d]]{2}.*\\.html$"))
+files_target <- sort((file_list)[id_target])
+files_path <- paste0(file_dir, files_target)
+
+# chrome app path
+path_browser <- "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+
+# input and output argument
+path_input <- paste0("file:///", files_path) 
+out_dir <- "d://github/course-stat/public/slide-pdf/2022-02/"
+path_out <- paste0(out_dir, stringr::str_replace(files_target,"html", "pdf"))
+
+
+for (i in 4:length(files_target)) {
+  pagedown::chrome_print(input = path_input[i],output = path_out[i] ,format = "pdf",
+                         browser = path_browser,timeout = 120)
+  Sys.sleep(0.5)
+  print(glue::glue("convert {i} / {length(files_target)} file: {files_target[i]} successed!"))
+}
+
---
+++ R/convert-pdf2pptx.R
@@ -0,0 +1,51 @@
+
+# see github [repo](https://github.com/jirilukavsky/pdf2pptx)
+
+#devtools::install_github("jirilukavsky/pdf2pptx")
+
+
+require(pdf2pptx)
+
+file_dir <- "d://github/course-stat/public/slide-pdf/2022-02/"
+file_list <- list.files(file_dir)
+id_target <- which(stringr::str_detect(file_list, "^[[\\d]]{2}.*\\.pdf$"))
+files_target <- sort((file_list)[id_target])
+files_path <- paste0(file_dir, files_target)
+
+path_out_43 <- paste0(file_dir, stringr::str_replace(files_target,"\\.pdf", "\\-43\\.pptx"))
+path_out_169 <- paste0(file_dir, stringr::str_replace(files_target,"\\.pdf", "\\-169\\.pptx"))
+
+# tem dir
+dir_tem <- "d://github/course-stat/public/slide-pdf/tem/"
+if (!dir.exists(dir_tem)) dir.create(dir_tem)
+
+# loop to convert
+# this will take long time, maybe 2~5 minutes per 100 slides.
+ratio <- 169
+for (i in 2:2) {
+  if (ratio ==169) {
+    pdf2pptx::pdf2pptx(pdf_filename =files_path[i],
+                       pptx_filename = path_out_169[i],
+                       ratio = ratio,
+                       path = dir_tem)
+  } else {
+    pdf2pptx::pdf2pptx(pdf_filename =files_path[i],
+                       pptx_filename = path_out_43[i],
+                       ratio = ratio,
+                       path = dir_tem)
+  }
+  
+  # clean tem dir
+  # get all files in the directories, recursively
+  f <- list.files(dir_tem, 
+                  include.dirs = F, 
+                  full.names = T, recursive = T)
+  # remove the files
+  file.remove(f)
+  
+  # print info
+  Sys.sleep(0.5)
+  print(glue::glue("convert {i} / {length(files_target)} file on ratio of {ratio}: {files_target[i]} successed!"))
+}
+
+
